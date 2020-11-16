d5e503e6bc5c46dc2fd3f5369e0c468274e65c8e
KiloSat <dipeshssatav@gmail.com>
2020-11-16 17:00:12 +0530

    Commit_1

---
+++ index.js
@@ -0,0 +1,28 @@
+console.log('Hello There Again');
+let num1= 'Dipesh';
+let num2= "Satav";
+const num3 = 43;
+console.log(num2,num1,num3);
+
+let name ='Gaurav';
+
+let person = {
+name1 :'Kolarov',
+num : 12, 
+};
+
+let arr = ['Cool',1,2,3,4,5];
+arr[9]='Bool'
+console.log(arr);
+
+console.log(person.num);
+
+function addition(v1,v2,v3){
+console.log(v1+v2+v3);  
+}
+
+addition(12,34,53);
+
+console.time(10);
+
+console.log("GTFO");
